d03ad5bd90aa1763e75139135cdadb06e886bb2e
AshutoshPearl <ashutosh@pearlorganisation.com>
2024-01-22 12:23:50 +0530

    First commit

---
+++ lib/constant/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF1F2260);
+  static const Color secondary = Color(0xFF484BB6);
+  static const Color text = Color(0xFF2F2F2F);
+  static const Color accent = Color(0xFFFFCD68);
+}
